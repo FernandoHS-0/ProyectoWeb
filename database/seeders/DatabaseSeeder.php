bf49b953b00ea8c9ecb47f5772e9a66d1de35624
Fernando Hernández <59585701+FernandoHS-0@users.noreply.github.com>
2021-04-16 16:21:54 -0500

    Listo para sprint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Administrador;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,5 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AlumnoTableSeeder::class);
         $this->call(MateriasTableSeeder::class);
         $this->call(ControlMateriasTableSeeder::class);
+        $this->call(PreRequisitosTableSeeder::class);
+        $this->call(AdministradorSeeder::class);
     }
 }
